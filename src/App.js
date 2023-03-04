20b866896cd190982651d44bfffdec5e0594e35b
jhonnycave <luis.j.cueva@gmail.com>
2023-03-03 22:16:26 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider, AuthRoute } from "./provider/auth";
 import Menu from "./nav/Menu";
+import BlogPage from "./page/BlogPage";
+import BlogPost from "./page/BlogPost";
 import HomePage from "./page/HomePage";
-import { AuthProvider } from "./provider/auth";
+import LoginPage from "./page/LoginPage";
+import LogoutPage from "./page/LogoutPage";
+import ProfilePage from "./page/ProfilePage";
 
 const App = () => {
     return (
@@ -12,6 +17,20 @@ const App = () => {
                     <Menu></Menu>
                     <Routes>
                         <Route path="/" element={<HomePage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/logout" element={<LogoutPage />} />
+                        <Route path="/blog" element={<BlogPage />}>
+                            <Route path=":slug" element={<BlogPost />} />
+                        </Route>
+                        <Route
+                            path="/profile"
+                            element={
+                                <AuthRoute>
+                                    <ProfilePage />
+                                </AuthRoute>
+                            }
+                        />
+                        <Route path="*" element={<p> Not Found</p>} />
                     </Routes>
                 </AuthProvider>
             </HashRouter>
--- src/nav/Menu.jsx
+++ src/nav/Menu.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../provider/auth";
+
+const routes = [];
+const r1 = {
+    to: "/",
+    text: "Home",
+    private: false,
+};
+const r2 = {
+    to: "/blog",
+    text: "Blog",
+    private: false,
+};
+const r3 = {
+    to: "/profile",
+    text: "Profile",
+    private: true,
+};
+const r4 = {
+    to: "/login",
+    text: "Login",
+    private: false,
+    publicOnly: true,
+};
+const r5 = {
+    to: "/logout",
+    text: "Logout",
+    private: true,
+};
+
+routes.push(r1);
+routes.push(r2);
+routes.push(r3);
+routes.push(r4);
+routes.push(r5);
 
 const Menu = () => {
-    return <div>Menu</div>;
+    const auth = useAuth();
+    const style = ({ isActive }) => ({
+        color: isActive ? "green" : "blue",
+    });
+
+    return (
+        <nav>
+            <ul>
+                {routes.map((route) => {
+                    if (route.private && !auth.user) return null;
+                    if (route.publicOnly && auth.user) return null;
+                    return (
+                        <li key={route.to}>
+                            <NavLink to={route.to} style={style}>
+                                {route.text}
+                            </NavLink>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
 };
 
 export default Menu;
