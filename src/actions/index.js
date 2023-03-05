24f41bdaa8146238f32147154f977a94bdf01e01
jhonnycave <luis.j.cueva@gmail.com>
2023-03-04 22:53:28 -0500

    macbook

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 export const setFavorite = (payload) => ({ type: "", payload });
-export const setFavorite = (payload) => ({ type: "", payload });
+export const deleteFavorite = (payload) => ({ type: "", payload });
 
-export const setFavorite = (payload) => ({ type: "", payload });
-export const setFavorite = (payload) => ({ type: "", payload });
+export const loginRequest = (payload) => ({ type: "", payload });
+export const logoutRequest = (payload) => ({ type: "", payload });
 export const setFavorite = (payload) => ({ type: "", payload });
 export const setFavorite = (payload) => ({ type: "", payload });
