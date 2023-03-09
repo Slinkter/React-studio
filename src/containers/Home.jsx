7cbe080e53c5dd9f089aa952dbcbfe2a4ddaa56e
jhonnycave <luis.j.cueva@gmail.com>
2023-03-09 10:42:45 -0500

    ok

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Categories from "../components/Categories";
+import Header from "../components/Header";
+import Search from "../components/Search";
 
 const mapStateToProps = (state) => {
     return {
@@ -15,15 +17,44 @@ const mapStateToProps = (state) => {
 const Home = (props) => {
     const { myList, trend, originals } = props;
 
-    const l01 = (
-        <Categories>
+    const listFavorite = (
+        <Categories title="Mi List">
             <Carousel>
-                <CarouselItem />
+                {myList.map((item) => (
+                    <CarouselItem key={item.id} {...item} isList />
+                ))}
             </Carousel>
         </Categories>
     );
 
-    return <React.Fragment>{l01}</React.Fragment>;
+    const listTrend = (
+        <Categories title="Tendencias">
+            <Carousel>
+                {trend.map((item) => (
+                    <CarouselItem key={item.id} {...item} isList />
+                ))}
+            </Carousel>
+        </Categories>
+    );
+    const listOriginals = (
+        <Categories title="Orginales">
+            <Carousel>
+                {originals.map((item) => (
+                    <CarouselItem key={item.id} {...item} isList />
+                ))}
+            </Carousel>
+        </Categories>
+    );
+
+    return (
+        <React.Fragment>
+            <Header />
+            <Search isHome />
+            {myList.length > 0 && listFavorite}
+            {trend.length > 0 && listTrend}
+            {originals.length > 0 && listOriginals}
+        </React.Fragment>
+    );
 };
 
 export default connect(mapStateToProps, null)(Home);
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { loginRequest } from "../actions/index";
 
-const Login = () => {
+const mapDispatchToPros = {
+    loginRequest,
+};
+
+const Login = (props) => {
+    const { loginRequest } = props;
+    const navigate = useNavigate();
+    const initUser = {
+        email: "",
+        password: "",
+    };
+
+    const [form, setValue] = useState(initUser);
+    const handleInput = (e) => {
+        const newForm = {
+            ...form,
+            [e.tarjet.name]: e.target.value,
+        };
+        setValue(newForm);
+    };
+
     return <div></div>;
 };
 
-export default Login;
+export default connect(null, mapDispatchToPros)(Login);
