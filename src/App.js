b044e97d8fb5a4de17f40461916b9c39a4d49a63
jhonnycave <luis.j.cueva@gmail.com>
2023-03-17 10:38:04 -0500

    reducer 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { initState } from "./components/db";
+import { reducer } from "./components/reducer";
+import { types } from "./components/types";
 
 const App = () => {
+    const [state, dispatch] = useReducer(reducer, initState);
+
+    const btnIncremet = () => {
+        dispatch({ type: types.increment, payload: 1 });
+    };
+    const btnDecrease = () => {
+        dispatch({ type: types.decrease, payload: 1 });
+    };
+    const btnReset = () => {
+        dispatch({ type: types.reset });
+    };
+
     return (
         <React.Fragment>
-            <h1>aPP</h1>
+            <h1>Count : {state.count}</h1>
+            <div>
+                <button onClick={btnIncremet}>+</button>
+                <button onClick={btnDecrease}>-</button>
+                <button onClick={btnReset}>0</button>
+            </div>
         </React.Fragment>
     );
 };
